d9eeaad2f34e60592215640ec0990c20137ca970
Ben Gruver <jesusfreke@jesusfreke.com>
2011-07-05 21:02:09 -0400

    Initial creation of Penroser project from IDEA

---
+++ src/org/jf/Penroser/Penroser.java
@@ -0,0 +1,15 @@
+package org.jf.Penroser;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Penroser extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
