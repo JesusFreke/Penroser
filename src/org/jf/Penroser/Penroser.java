4b84ed63300c4aeaa6352bd00df17e25580ed3b0
Ben Gruver <jesusfreke@jesusfreke.com>
2011-08-25 01:21:50 -0400

    Allow user to toggle full screen mode in the activity view

--- src/org/jf/Penroser/Penroser.java
+++ src/org/jf/Penroser/Penroser.java
@@ -2,6 +2,7 @@ package org.jf.Penroser;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.*;
 
 import java.util.Random;
 
@@ -11,13 +12,43 @@ public class Penroser extends Activity
 
     public static final HalfRhombusPool halfRhombusPool = new HalfRhombusPool();
 
+    private boolean fullScreen = false;
+
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
 
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+
         PenroserGLView penroserGLView = new PenroserGLView(this);
         setContentView(penroserGLView);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.full_screen:
+                toggleFullScreen();
+                return true;
+        }
+        return false;
+    }
+
+    private void toggleFullScreen() {
+        if (fullScreen) {
+            getWindow().clearFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        } else {
+            getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        }
+        fullScreen = !fullScreen;
     }
 }
