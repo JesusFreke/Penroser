6c86a3c15323250a29300bad872fe0d4a6dc6d92
Ben Gruver <jesusfreke@jesusfreke.com>
2011-09-02 23:23:14 -0400

    Fix a crash on vibrant, related to the construction of the wallpaper/glsurfaceview

--- src/org/jf/GLWallpaper/GLWallpaperService.java
+++ src/org/jf/GLWallpaper/GLWallpaperService.java
@@ -32,95 +32,274 @@ import android.opengl.GLSurfaceView;
 import android.service.wallpaper.WallpaperService;
 import android.view.SurfaceHolder;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public abstract class GLWallpaperService extends WallpaperService {
     public class GLEngine extends WallpaperService.Engine {
-        private GLSurfaceView mGLSurfaceView;
+        private Object lock = new Object();
+        private GLSurfaceView mGLSurfaceView = null;
+
+        private int debugFlags;
+        private int renderMode;
+
+        /**
+         * If we don't have a GLSurfaceView yet, then we queue up any operations that are requested, until the
+         * GLSurfaceView is created.
+         *
+         * Initially, we created the glSurfaceView in the GLEngine constructor, and things seemed to work. However,
+         * it turns out a few devices aren't set up to handle the surface related events at this point, and crash.
+         *
+         * This is a work around so that we can delay the creation of the GLSurfaceView until the surface is actually
+         * created, so that the underlying code should be in a state to be able to handle the surface related events
+         * that get fired when GLSurfaceView is created.
+         */
+        private List<Runnable> pendingOperations = new ArrayList<Runnable>();
 
         public GLEngine() {
-            mGLSurfaceView = new GLWallpaperSurfaceView(GLWallpaperService.this);
         }
 
-        public void setGLWrapper(GLSurfaceView.GLWrapper glWrapper) {
-            mGLSurfaceView.setGLWrapper(glWrapper);
+        public void setGLWrapper(final GLSurfaceView.GLWrapper glWrapper) {
+            synchronized (lock) {
+                if (mGLSurfaceView != null) {
+                    mGLSurfaceView.setGLWrapper(glWrapper);
+                } else {
+                    pendingOperations.add(new Runnable() {
+                        public void run() {
+                            setGLWrapper(glWrapper);
+                        }
+                    });
+                }
+            }
         }
 
-        public void setDebugFlags(int debugFlags) {
-            mGLSurfaceView.setDebugFlags(debugFlags);
+        public void setDebugFlags(final int debugFlags) {
+            synchronized (lock) {
+                if (mGLSurfaceView != null) {
+                    mGLSurfaceView.setDebugFlags(debugFlags);
+                } else {
+                    this.debugFlags = debugFlags;
+                    pendingOperations.add(new Runnable() {
+                        public void run() {
+                            setDebugFlags(debugFlags);
+                        }
+                    });
+                }
+            }
         }
 
         public int getDebugFlags() {
-            return mGLSurfaceView.getDebugFlags();
-        }
-
-        public void setRenderer(GLSurfaceView.Renderer renderer) {
-            mGLSurfaceView.setRenderer(renderer);
-            if (!isVisible()) {
-                mGLSurfaceView.onPause();
+            synchronized (lock) {
+                if (mGLSurfaceView != null) {
+                    return mGLSurfaceView.getDebugFlags();
+                } else {
+                    return debugFlags;
+                }
             }
         }
 
-        public void setEGLContextFactory(GLSurfaceView.EGLContextFactory factory) {
-            mGLSurfaceView.setEGLContextFactory(factory);
+        public void setRenderer(final GLSurfaceView.Renderer renderer) {
+            synchronized (lock) {
+                if (mGLSurfaceView != null) {
+                    mGLSurfaceView.setRenderer(renderer);
+                    if (!isVisible()) {
+                        mGLSurfaceView.onPause();
+                    }
+                } else {
+                    pendingOperations.add(new Runnable() {
+                        public void run() {
+                            setRenderer(renderer);
+                        }
+                    });
+                }
+            }
         }
 
-        public void setEGLWindowSurfaceFactory(GLSurfaceView.EGLWindowSurfaceFactory factory) {
-            mGLSurfaceView.setEGLWindowSurfaceFactory(factory);
+        public void setEGLContextFactory(final GLSurfaceView.EGLContextFactory factory) {
+            synchronized (lock) {
+                if (mGLSurfaceView != null) {
+                    mGLSurfaceView.setEGLContextFactory(factory);
+                } else {
+                    pendingOperations.add(new Runnable() {
+                        public void run() {
+                            setEGLContextFactory(factory);
+                        }
+                    });
+                }
+            }
         }
 
-        public void setEGLConfigChooser(GLSurfaceView.EGLConfigChooser configChooser) {
-            mGLSurfaceView.setEGLConfigChooser(configChooser);
+        public void setEGLWindowSurfaceFactory(final GLSurfaceView.EGLWindowSurfaceFactory factory) {
+            synchronized (lock) {
+                if (mGLSurfaceView != null) {
+                    mGLSurfaceView.setEGLWindowSurfaceFactory(factory);
+                } else {
+                    pendingOperations.add(new Runnable() {
+                        public void run() {
+                            setEGLWindowSurfaceFactory(factory);
+                        }
+                    });
+                }
+            }
         }
 
-        public void setEGLConfigChooser(boolean needDepth) {
-            mGLSurfaceView.setEGLConfigChooser(needDepth);
+        public void setEGLConfigChooser(final GLSurfaceView.EGLConfigChooser configChooser) {
+            synchronized (lock) {
+                if (mGLSurfaceView != null) {
+                    mGLSurfaceView.setEGLConfigChooser(configChooser);
+                } else {
+                    pendingOperations.add(new Runnable() {
+                        public void run() {
+                            setEGLConfigChooser(configChooser);
+                        }
+                    });
+                }
+            }
         }
 
-        public void setEGLConfigChooser(int redSize, int greenSize, int blueSize,
-            int alphaSize, int depthSize, int stencilSize) {
-            mGLSurfaceView.setEGLConfigChooser(redSize, greenSize, blueSize,
-                    alphaSize, depthSize, stencilSize);
+        public void setEGLConfigChooser(final boolean needDepth) {
+            synchronized (lock) {
+                if (mGLSurfaceView != null) {
+                    mGLSurfaceView.setEGLConfigChooser(needDepth);
+                } else {
+                    pendingOperations.add(new Runnable() {
+                        public void run() {
+                            setEGLConfigChooser(needDepth);
+                        }
+                    });
+                }
+            }
         }
 
-        public void setEGLContextClientVersion(int version) {
-            mGLSurfaceView.setEGLContextClientVersion(version);
+        public void setEGLConfigChooser(final int redSize, final int greenSize, final int blueSize,
+            final int alphaSize, final int depthSize, final int stencilSize) {
+            synchronized (lock) {
+                if (mGLSurfaceView != null) {
+                    mGLSurfaceView.setEGLConfigChooser(redSize, greenSize, blueSize,
+                        alphaSize, depthSize, stencilSize);
+                } else {
+                    pendingOperations.add(new Runnable() {
+                        public void run() {
+                            setEGLConfigChooser(redSize, greenSize, blueSize, alphaSize, depthSize, stencilSize);
+                        }
+                    });
+                }
+            }
         }
 
-        public void setRenderMode(int renderMode) {
-            mGLSurfaceView.setRenderMode(renderMode);
+        public void setEGLContextClientVersion(final int version) {
+            synchronized (lock) {
+                if (mGLSurfaceView != null) {
+                    mGLSurfaceView.setEGLContextClientVersion(version);
+                } else {
+                    pendingOperations.add(new Runnable() {
+                        public void run() {
+                            setEGLContextClientVersion(version);
+                        }
+                    });
+                }
+            }
+        }
+
+        public void setRenderMode(final int renderMode) {
+            synchronized (lock) {
+                if (mGLSurfaceView != null) {
+                    mGLSurfaceView.setRenderMode(renderMode);
+                } else {
+                    this.renderMode = renderMode;
+                    pendingOperations.add(new Runnable() {
+                        public void run() {
+                            setRenderMode(renderMode);
+                        }
+                    });
+                }
+            }
         }
 
         public int getRenderMode() {
-            return mGLSurfaceView.getRenderMode();
+            synchronized (lock) {
+                if (mGLSurfaceView != null) {
+                    return mGLSurfaceView.getRenderMode();
+                } else {
+                    return renderMode;
+                }
+            }
         }
 
         public void requestRender() {
-            mGLSurfaceView.requestRender();
-        }
-
-        @Override
-        public void onVisibilityChanged(boolean visible) {
-            super.onVisibilityChanged(visible);
-
-            if (visible) {
-                mGLSurfaceView.onResume();
-            } else {
-                mGLSurfaceView.onPause();
+            if (mGLSurfaceView != null) {
+                mGLSurfaceView.requestRender();
             }
         }
 
         @Override
-        public void onSurfaceChanged(SurfaceHolder holder, int format, int width, int height) {
-            mGLSurfaceView.surfaceChanged(holder, format, width, height);
+        public void onVisibilityChanged(final boolean visible) {
+            super.onVisibilityChanged(visible);
+
+            synchronized (lock) {
+                if (mGLSurfaceView != null) {
+                    if (visible) {
+                        mGLSurfaceView.onResume();
+                    } else {
+                        mGLSurfaceView.onPause();
+                    }
+                } else {
+                    pendingOperations.add(new Runnable() {
+                        public void run() {
+                            if (visible) {
+                                mGLSurfaceView.onResume();
+                            } else {
+                                mGLSurfaceView.onPause();
+                            }
+                        }
+                    });
+                }
+            }
+        }
+
+        @Override
+        public void onSurfaceChanged(final SurfaceHolder holder, final int format, final int width, final int height) {
+            synchronized (lock) {
+                if (mGLSurfaceView != null) {
+                    mGLSurfaceView.surfaceChanged(holder, format, width, height);
+                } else {
+                    pendingOperations.add(new Runnable() {
+                        public void run() {
+                            onSurfaceChanged(holder, format, width, height);
+                        }
+                    });
+                }
+            }
         }
 
         @Override
         public void onSurfaceCreated(SurfaceHolder holder) {
-            mGLSurfaceView.surfaceCreated(holder);
+            synchronized (lock) {
+                if (mGLSurfaceView == null) {
+                    mGLSurfaceView = new GLSurfaceView(GLWallpaperService.this) {
+                        @Override
+                        public SurfaceHolder getHolder() {
+                            return GLEngine.this.getSurfaceHolder();
+                        }
+                    };
+                    for (Runnable pendingOperation: pendingOperations) {
+                        pendingOperation.run();
+                    }
+                    pendingOperations.clear();
+                }
+                mGLSurfaceView.surfaceCreated(holder);
+            }
         }
 
         @Override
         public void onSurfaceDestroyed(SurfaceHolder holder) {
-            mGLSurfaceView.surfaceDestroyed(holder);
+            synchronized (lock) {
+                if (mGLSurfaceView != null) {
+                    mGLSurfaceView.surfaceDestroyed(holder);
+                }
+                mGLSurfaceView = null;
+                pendingOperations.clear();
+            }
         }
     }
 }
